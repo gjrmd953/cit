5331cf5edb85c7f96441c2af2d5178fc0e6682bc
gjrmd953 <gjrmd953@gmail.com>
2026-01-15 17:08:10 +0600

    edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import { IoCall } from 'react-icons/io5';
 import { MdOutlineEmail } from 'react-icons/md';
+import Container from './Container';
 
 const Header = () => {
-  return (
-    <div className="py-3 bg-[#ff1e1e]">
-      <div className="container mx-auto px-4 flex items-center justify-between">
-        <div className="flex gap-5">
-          <div className="flex items-center gap-2 font-INT">
-            <IoCall className="text-white text-[12px]" />
-            <h2 className="!text-[12px] text-white leading-none">08767856454</h2>
-          </div>
-          <div className="flex items-center gap-2">
-            <MdOutlineEmail className="text-white text-[12px]" />
-            <h3 className="!text-[16px] text-white leading-none">
-              info@creativeitinstitute.com
-            </h3>
-          </div>
+    return (
+        <div className="py-3 bg-[#ff1e1e]">
+            <Container>
+                <div className="flex justify-between">
+                    <div className="w-1/2 flex gap-5">
+                        <div className="flex items-center gap-2 font-INT">
+                            <IoCall className="text-white text-2xl" />
+                            <h2 className="!text-[16px] text-white leading-none"> 01777308777 </h2>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <MdOutlineEmail className="text-white text-2xl" />
+                            <h3 className="!text-[16px] text-white leading-none">
+                                info@creativeitinstitute.com
+                            </h3>
+                        </div>
+                    </div>
+                    <div className="w-1/2">
+                        <div className="flex justify-end gap-2.5 font-INT text-white">
+                            <h2>BN</h2>
+                            <h3>EN</h3>
+                        </div>
+                    </div>
+                </div>
+            </Container>
         </div>
-        <div className="w-1/2">{/* Future content */}</div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Header;
