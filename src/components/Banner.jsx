6ed7d8bb1dd479cbab8421a81db8cc740825cf37
gjrmd953 <gjrmd953@gmail.com>
2026-01-21 18:57:28 +0600

    edit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import Container from './Container'
 import BanImg from "../assets/banner.png"
+import ThekmarkImg from "../assets/thekmark.png"
 
 const Banner = () => {
   return (
     <div className='py-7'>
-        <Container>
-            <div className="flex justify-between">
-                <div className="w-1/3">
-                   <h2>hello</h2>
-                </div>
-                <div className="w-2/3">
-                  <img src={BanImg} alt="" />
-                </div>
+      <Container>
+        <div className="flex justify-between">
+          <div className="w-2/5">
+            <div className="">
+              <div className="flex gap-3">
+                <img src={ThekmarkImg} alt="" />
+                <h2 className='font-INT font-black text-[18px] text-[#1f1e1e]'>
+                  Unleash Your Potential
+                </h2>
+              </div>
+              <h2 className='font-INT font-bold text-5xl py-3.5 leading-tight'>
+                Become an IT Pro & <br /> Rule the <span className='text-[rgb(255,30,30)]'>Digital World</span>
+              </h2>
+              <p className='font-INT text-[#212529]'>
+                With a vision to turn manpower into assets, Creative IT <br /> Institute is ready to enhance your learning experience with <br /> skilled mentors and an updated curriculum. Pick your desired <br /> course from more than 45 trendy options.
+              </p>
             </div>
-        </Container>
+          </div>
+          <div className="w-3/5">
+            <img src={BanImg} alt="" />
+          </div>
+        </div>
+      </Container>
     </div>
   )
 }
