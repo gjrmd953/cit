849bef9b8b98b8963556c90e05796c31a6d3f1fc
gjrmd953 <gjrmd953@gmail.com>
2026-02-02 23:25:27 +0600

    edit

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 import Container from './Container'
 
 const Courses = () => {
-  return (
-    <div>
-        <Container>
-            <div className="">
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-            </div>
-        </Container>
-    </div>
-  )
+    return (
+        <div>
+            <Container>
+                <div className="">
+                    <div className="">
+                        <h2>
+                            Networking &amp; Cyber Security
+                        </h2>
+                    </div>
+                    <div className=""></div>
+                    <div className=""></div>
+                    <div className=""></div>
+                    <div className=""></div>
+                    <div className=""></div>
+                </div>
+            </Container>
+        </div>
+    )
 }
 
 export default Courses
